9eb107579dd5aab161346e93b9208a7688cc0fe0
NazaR421 <131592318+NazaR421@users.noreply.github.com>
2024-04-08 18:00:47 +0300

    Update Shooter.py

--- Shooter.py
+++ Shooter.py
@@ -1,4 +1,7 @@
+from random import randint
+from typing import Any
 from pygame import*
+
 
 mixer.init()
 mixer.music.load("space.ogg")
@@ -7,6 +10,15 @@ fire_sound=mixer.Sound("fire.ogg")
 
 img_back="galaxy.jpg"
 img_rocket="rocket.png"
+img_ufo="ufo.png"
+img_fire="bullet.png"
+
+font.init()
+font1=font.Font(None,36)
+score=0
+lost=0
+
+
 
 
 class GameSprite(sprite.Sprite):
@@ -29,10 +41,40 @@ class Player(GameSprite):
         if keys[K_RIGHT]and self.rect.x < 610:
             self.rect.x += self.speed
 
+    def fire(self):
+        bullet=Bullet(img_fire,self.rect.centerx,self.rect.top,15,20,-15)
+        bullets.add(bullet)
+
+bullets=sprite.Group()
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y+=self.speed
+        if self.rect.y<0:
+            self.kill()
+
+
+class Enemy(GameSprite):
+    def update(self):
+        self.rect.y+=self.speed
+        global lost
+        if self.rect.y>500:
+            self.rect.x=randint(80,620)
+            self.rect.y=0
+            lost=lost+1
+        
+
 window=display.set_mode((700,500))
 background=transform.scale(image.load(img_back),(700,500))
 
 ship=Player(img_rocket,5,400,80,100,10)
+ufos=sprite.Group()
+for i in range(1,6):
+    ufo=Enemy(img_ufo,randint(80,620),-30,80,50,randint(1,5))
+    ufos.add(ufo)
+
+
+
+
 run=True
 finish=False
 
@@ -40,10 +82,26 @@ while run:
     for e in event.get():
         if e.type ==QUIT:
             run=False
+        elif e.type==KEYDOWN:
+            if e.key==K_SPACE:
+                ship.fire()
+
     if not finish:
         window.blit(background,(0,0))
+        text=font1.render("РАХУНОК:"+str(score),1,(255,255,255))
+        window.blit(text,(10,20))
+        text_lost=font1.render("ПРОПУЩЕНО:"+str(lost),1,(255,255,255))
+        window.blit(text_lost,(10,50))
+        
+
+
         ship.draw()
         ship.move()
+        ufos.update()
+        ufos.draw(window)
+        bullets.update()
+        bullets.draw(window)
+
 
 
 
