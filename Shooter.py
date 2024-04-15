de14ebe22f96eb3b80ae4965cdbbcc9dd7d926c5
NazaR421 <131592318+NazaR421@users.noreply.github.com>
2024-04-15 17:46:21 +0300

    Update Shooter.py

--- Shooter.py
+++ Shooter.py
@@ -1,7 +1,7 @@
 from random import randint
 from typing import Any
 from pygame import*
-
+from time import time as timer
 
 mixer.init()
 mixer.music.load("space.ogg")
@@ -15,6 +15,9 @@ img_fire="bullet.png"
 
 font.init()
 font1=font.Font(None,36)
+font2=font.Font(None,80)
+win =font2.render("YOU WiIN!",True,(117, 242, 7))
+lose =font2.render("YOU LOSE!",True,(180,0,0))
 score=0
 lost=0
 
@@ -77,6 +80,9 @@ for i in range(1,6):
 
 run=True
 finish=False
+num_fire=0
+rel_time=False
+
 
 while run:
     for e in event.get():
@@ -84,7 +90,13 @@ while run:
             run=False
         elif e.type==KEYDOWN:
             if e.key==K_SPACE:
-                ship.fire()
+                if num_fire<5 and rel_time==False:
+                    num_fire=num_fire+1
+                    ship.fire()
+                if num_fire>=5 and rel_time==False:
+                    last_time=timer()
+                    rel_time=True
+
 
     if not finish:
         window.blit(background,(0,0))
@@ -102,8 +114,27 @@ while run:
         bullets.update()
         bullets.draw(window)
 
+        if rel_time == True:
+            now_time=timer()
+            if now_time-last_time<1 :
+                load=font1.render("Почекай,релоад...",1,(150,0,0))
+                window.blit(load,(250,450))
+            else:
+                num_fire=0
+                rel_time=False
+        collides=sprite.groupcollide(ufos,bullets,True,True)
+        for c in collides:
+            score=score+1
+            ufo=Enemy(img_ufo,randint(80,620),-30,80,50,randint(1,5))
+            ufos.add(ufo)
+        
 
-
-
+        if sprite.spritecollide(ship,ufos,False) or lost >=10:
+            finish=True
+            window.blit(lose,(200,200))
+        if score>=15:
+            finish=False
+            window.blit(win,(200,200))
+            
         display.update()
     time.delay(40)
